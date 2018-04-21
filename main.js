214a237a2cfc0d971d22c04b4a6d5fcc768337bd
Nick Scheel <sche0210@stthomas.edu>
2018-04-21 14:52:16 -0500

    testing

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 
+
+
+    $.get("https://www.barcodelookup.com/restapi?barcode=9780140157376&formatted=y&key=xl7mgwb7icvriqtl9qhiipdzaxsjld", function (data, status) {
+        console.log("Data: " + data + "\nStatus: " + status);
+    });
+
 document.addEventListener("keypress", function(e){
     if (e.keyCode == 13) {
         var code = document.getElementById('input').value;
